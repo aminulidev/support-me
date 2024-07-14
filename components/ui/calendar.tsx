b313db22699604f16f015d24720f37f0d547c8f2
aminulidev <aminulds61@gmail.com>
2024-07-14 08:42:17 +0600

    calender continue

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -6,6 +6,8 @@ import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
+import {format} from "date-fns";
+import {Select, SelectContent, SelectItem, SelectTrigger} from "@/components/ui/select";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
@@ -23,7 +25,7 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-sm font-medium hidden",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -56,6 +58,26 @@ function Calendar({
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Dropdown: (dropdownProps) => {
+            let selectValues: {value: string; label: string}[] = [];
+
+            if (dropdownProps.name === "months") {
+                selectValues = Array.from({length: 12}, (_, i) => {
+                    return {
+                        value: i.toString(),
+                        label: format(new Date(new Date().getFullYear(), i, 1), "MMM"),
+                    };
+                });
+            }
+            return <Select>
+                <SelectTrigger>Dropdown</SelectTrigger>
+                <SelectContent>
+                    {selectValues.map(selectValue => (
+                        <SelectItem key={selectValue.value} value={selectValue.value}>{selectValue.label}</SelectItem>
+                    ))}
+                </SelectContent>
+            </Select>
+        }
       }}
       {...props}
     />
