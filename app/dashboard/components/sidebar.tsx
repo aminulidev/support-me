88ca30f53f9445fa56adb262e6555e19c0a6bb11
aminulidev <aminulds61@gmail.com>
2024-07-26 08:51:39 +0600

    mobile menu done

--- app/dashboard/components/sidebar.tsx
+++ app/dashboard/components/sidebar.tsx
@@ -4,15 +4,18 @@ import MenuItem from "@/app/dashboard/components/menu-item";
 import {Avatar, AvatarFallback} from "@/components/ui/avatar";
 import Link from "next/link";
 import LightDarkToggle from "@/components/ui/light-dark-toggle";
+import {cn} from "@/lib/utils";
 
-const Sidebar = () => {
+const Sidebar = ({ className }: { className?: string }) => {
     return (
-        <nav className="bg-muted overflow-auto p-4 flex flex-col">
-            <header className="border-b dark:border-b-black border-b-zinc-300 pb-4">
-                <MenuTitle />
+        <nav
+            className={cn(`md:bg-muted overflow-auto p-4 flex flex-col`, className)}
+        >
+            <header className="hidden md:block border-b dark:border-b-black border-b-zinc-300 pb-4">
+                <MenuTitle/>
             </header>
             <ul className="py-4 grow">
-                <MenuItem href="/dashboard">My Dashboard</MenuItem>
+                <MenuItem href="/dashboard">My dashboard</MenuItem>
                 <MenuItem href="/dashboard/teams">Teams</MenuItem>
                 <MenuItem href="/dashboard/employees">Employees</MenuItem>
                 <MenuItem href="/dashboard/account">Account</MenuItem>
@@ -20,10 +23,14 @@ const Sidebar = () => {
             </ul>
             <footer className="flex gap-2 items-center">
                 <Avatar>
-                    <AvatarFallback className="bg-primary">AP</AvatarFallback>
+                    <AvatarFallback className="bg-pink-300 dark:bg-pink-800">
+                        TP
+                    </AvatarFallback>
                 </Avatar>
-                <Link href="/" className="hover:underline">Logout</Link>
-                <LightDarkToggle className="ml-auto rounded-full" />
+                <Link href="/" className="hover:underline">
+                    Logout
+                </Link>
+                <LightDarkToggle className="ml-auto"/>
             </footer>
         </nav>
     );
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+"use client";
+import React, {useState} from 'react';
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import Sidebar from "@/app/dashboard/components/sidebar";
+import {useMediaQuery} from "@/hooks/use-media-query";
+import MenuTitle from "@/app/dashboard/components/menu-title";
+import {MenuIcon} from "lucide-react";
 
 const DashboardLayout = ({children}: {children: React.ReactNode}) => {
+    const isDesktop = useMediaQuery("(min-width: 768px)");
+
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const onCloseDrawer = () => {
+        setMobileMenuOpen(false);
+    }
+
     return (
-        <div className="grid grid-cols-[250px_1fr] h-screen">
-            <Sidebar />
+        <div className="grid md:grid-cols-[250px_1fr] h-screen">
+            <Sidebar className="hidden md:flex"/>
+            {/* mobile menu */}
+            {!isDesktop && (
+                <div className="p-4 flex justify-between md:hidden sticky top-0 right-0 bg-background border-b border-border h-fit">
+                    <MenuTitle />
+                    <Drawer
+                        direction="right"
+                        open={mobileMenuOpen}
+                        onClose={onCloseDrawer}
+                        onOpenChange={(open) => setMobileMenuOpen(open)}
+                    >
+                        <DrawerTrigger>
+                            <MenuIcon />
+                        </DrawerTrigger>
+                        <DrawerContent className="w-[320px] left-auto mt-0">
+                            <Sidebar />
+                        </DrawerContent>
+                    </Drawer>
+                </div>
+            )}
+
             <div className="overflow-auto py-2 px-4">
                 <h1 className="pb-4">Welcome back, Aminul</h1>
                 {children}
